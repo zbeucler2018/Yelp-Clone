ee840ea19d8e36d6e1e0bbc99789dc3d0ca4ca39
zbeucler2018 <zbeucler@gmail.com>
2021-01-29 23:05:51 -0500

    debugging

--- dist/youtube/server.js
+++ dist/youtube/server.js
@@ -5,10 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/zbeuclerFinal"));
+//app.use(express.static(__dirname + "/dist/zbeuclerFinal"));
+app.use(express.static(__dirname + "/dist/youtube"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/zbeuclerFinal/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/youtube/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
