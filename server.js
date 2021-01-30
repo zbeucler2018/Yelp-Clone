169e97cec434dde9c976989b4d0a3c232d80654d
zbeucler2018 <zbeucler@gmail.com>
2021-01-29 23:13:59 -0500

    changed file path in server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + "/dist/zbeuclerFinal"));
-app.use(express.static(__dirname + "/dist/youtube"));
+app.use(express.static(__dirname + "./dist/youtube"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/youtube/index.html"));
+  res.sendFile(path.join(__dirname + "./dist/youtube/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
